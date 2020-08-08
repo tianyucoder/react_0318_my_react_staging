49c80fcffd9b891ab9af7bec51f6e0331e11755d
tianyu5177 <zhangtianyu5177@sina.com>
2020-08-08 11:26:52 +0800

    更新

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ export default class App extends Component {
 		return (
 			<div className="container">
 				<Search updateAppState={this.updateAppState}/>
-				<List/>
+				<List {...this.state}/>
 			</div>
 		)
 	}
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -3,38 +3,35 @@ import './index.css'
 
 export default class List extends Component {
 	render() {
+		//获取App传递过来的状态数据
+		const {
+			isFirst,
+			isLoading,
+			error,
+			users,
+		} = this.props
+
 		return (
 			<div className="row">
-				<div className="card">
-					<a href="https://github.com/reactjs" target="_blank">
-						<img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2082848815,1382311806&fm=26&gp=0.jpg" style={{width: '100px'}}/>
-					</a>
-					<p className="card-text">reactjs</p>
-				</div>
-				<div className="card">
-					<a href="https://github.com/reactjs" target="_blank">
-						<img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2082848815,1382311806&fm=26&gp=0.jpg" style={{width: '100px'}}/>
-					</a>
-					<p className="card-text">reactjs</p>
-				</div>
-				<div className="card">
-					<a href="https://github.com/reactjs" target="_blank">
-						<img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2082848815,1382311806&fm=26&gp=0.jpg" style={{width: '100px'}}/>
-					</a>
-					<p className="card-text">reactjs</p>
-				</div>
-				<div className="card">
-					<a href="https://github.com/reactjs" target="_blank">
-						<img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2082848815,1382311806&fm=26&gp=0.jpg" style={{width: '100px'}}/>
-					</a>
-					<p className="card-text">reactjs</p>
-				</div>
-				<div className="card">
-					<a href="https://github.com/reactjs" target="_blank">
-						<img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2082848815,1382311806&fm=26&gp=0.jpg" style={{width: '100px'}}/>
-					</a>
-					<p className="card-text">reactjs</p>
-				</div>
+				{
+					isFirst ? //判断是否是初次展示
+						<h1>输入关键字，随后点击搜索</h1> :
+					isLoading ?//判断是否是加载中
+						<h1>loading.....</h1> :
+					error ? //判断是否有错误
+						<h1 style={{color:'red'}}>{error}</h1> :
+					//正常渲染列表
+					users.map((user)=>{
+						return (
+							<div key={user.id} className="card">
+								<a href={user.html_url} target="_blank">
+									<img src={user.avatar_url} style={{width: '100px'}}/>
+								</a>
+								<p className="card-text">{user.login}</p>
+							</div>
+						)
+					})
+				}
 			</div>
 		)
 	}
